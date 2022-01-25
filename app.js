be508225e7c92174282cbc13f327cb2fba070ffd
Jacegk1999 <Jacegk1999@gmail.com>
2022-01-25 09:41:44 -0800

    redid app.js to the same system im using in the html. Working correctly now.

--- app.js
+++ app.js
@@ -1,65 +1,79 @@
 // import functions and grab DOM elements
 
-const shell_1_Button = document.getElementById('shell_1_button');
-const shell_2_Button = document.getElementById('shell_2_button');
-const shell_3_Button = document.getElementById('shell_3_button');
+const ball1 = document.getElementById('ball_1');
+const ball2 = document.getElementById('ball_2');
+const ball3 = document.getElementById('ball_3');
 
-const shell_1_Container = document.getElementById('container');
-const shell_2_Container = document.getElementById('container');
-const shell_3_Container = document.getElementById('container');
+const button1 = document.getElementById('shell1_Button');
+const button2 = document.getElementById('shell2_Button');
+const button3 = document.getElementById('shell3_Button');
 
-const totalEl = document.getElementById('total');
-const lossesEl = document.getElementById('losses');
-const winsEl = document.getElementById('wins');
+const winsSpan = document.getElementById('wins');
+const lossesSpan = document.getElementById('losses');
+const totalSpan = document.getElementById('total');
 
-// initialize state
-const ball = ['shell_1', 'shell_2', 'shell_3'];
+let wins = 0;
+let total = 0;
 
-let correctGuesses = 0;
-let totalGuesses = 0;
+function resetStyle() {
+    ball1.classList.remove('reveal');
+    ball2.classList.remove('reveal');
+    ball3.classList.remove('reveal');
+}
 
-shell_1_Button.addEventListener('click', () => {
-    const hidingSpot = Math.floor(Math.random() * 3);
-    const answer = ball[hidingSpot];
-    handleGuess(answer, 'shell_1');
-});
+function displayResults() {
+    winsSpan.textContent = wins;
+    totalSpan.textContent = total;
+    lossesSpan.textContent = total - wins;
+}
 
-shell_2_Button.addEventListener('click', () => {
-    const hidingSpot = Math.floor(Math.random() * 3);
-    const answer = ball[hidingSpot];
-    handleGuess(answer, 'shell_2');
-});
-
-shell_3_Button.addEventListener('click', () => {
-    const hidingSpot = Math.floor(Math.random() * 3);
-    const answer = ball[hidingSpot];
-    handleGuess(answer, 'shell_2'); 
-});
-
-function handleGuess(userGuess, correctSpot) {
-    
-    resetStyles();
-    totalGuesses++;
-
-    const correctHidingPlaceEl = document.getElementById(`${userGuess}-container`);
-    
-    correctHidingPlaceEl.classList.add('reveal');
-    
-
-    if (userGuess === correctSpot) {
-        correctGuesses++;
+button1.addEventListener('click', () => {
+    total++;
+    resetStyle();
+    const ballLocation = Math.ceil(Math.random() * 3);
+    if (ballLocation === 1) {
+        ball1.classList.add('reveal');
+        wins++;
+    } else if (ballLocation === 2) {
+        ball2.classList.add('reveal');
+    } else {
+        ball3.classList.add('reveal');
     }
+    displayResults();
+});
 
-    totalEl.textContent = totalGuesses;
-    winsEl.textContent = correctGuesses;
-    lossesEl.textContent = totalGuesses - correctGuesses;
-}
+button2.addEventListener('click', () => {
+    total++;
+    resetStyle();
+    const ballLocation = Math.ceil(Math.random() * 3);
 
-function resetStyles() {
-    shell_1_Container.classList.remove('reveal');
-    shell_2_Container.classList.remove('reveal');
-    shell_3_Container.classList.remove('reveal');
-}
+    if (ballLocation === 1) {
+        ball1.classList.add('reveal');
+    } else if (ballLocation === 2) {
+        ball2.classList.add('reveal');
+        wins++;
+    } else {
+        ball3.classList.add('reveal');
+    }
+    displayResults();
+});
+
+button3.addEventListener('click', () => {
+    total++;
+    resetStyle();
+    const ballLocation = Math.ceil(Math.random() * 3);
+
+    if (ballLocation === 1) {
+        ball1.classList.add('reveal');
+    } else if (ballLocation === 2) {
+        ball2.classList.add('reveal');
+    } else {
+        ball3.classList.add('reveal');
+        wins++;
+    }
+    displayResults();
+});
+
 
 // let state
 
